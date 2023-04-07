7469b60106021ed924898397004d9839148031ac
choideakook <shdrnrhd113@naver.com>
2023-04-07 21:57:03 +0900

    Rq 구현

--- src/main/java/com/lldj/gram/boundedContext/member/MemberService.java
+++ src/main/java/com/lldj/gram/boundedContext/member/MemberService.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public Optional<Object> findByUsername(String username) {
+        return null;
+    }
 
     //-- Security Join --//
 
